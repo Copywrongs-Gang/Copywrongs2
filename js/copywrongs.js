e500f215f364e09cf3564dcd9548e4a7831d5c6b
c3o <github@c3o.org>
2015-06-07 14:16:39 +0200

    Language detection: Don't do anything if browser lang is english

--- js/copywrongs.js
+++ js/copywrongs.js
@@ -5,14 +5,16 @@ window.onload = function() {
 	if (document.location.pathname == '/') {
 		var browserLang = navigator.languages ? navigator.languages[0] : (navigator.language || navigator.userLanguage);
 		browserLang = browserLang.substr(0,2);
-		var oo = document.getElementById('langselect').options;
-		for (var i=0; i<oo.length; i++) {
-			if (oo[i].value == browserLang) { // if it exists in the language dropdown
-				document.location.replace('/'+browserLang);
+		if (browserLang != 'en') { // don't need to do anything there
+			var oo = document.getElementById('langselect').options;
+			for (var i=0; i<oo.length; i++) {
+				if (oo[i].value == browserLang) { // if it exists in the language dropdown
+					document.location.replace('/'+browserLang);
+				}
 			}
 		}
 	}
-	
+
 	// "don't call now" overlay
 	var d = new Date();
 	if (d.getUTCDay() === 0 || // if Sunday
